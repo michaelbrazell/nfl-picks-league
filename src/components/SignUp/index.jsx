d1af7dc880acd32be38fd518cc052b9f65344e6f
Mike Murray <michaelbrazell@gmail.com>
2020-01-01 23:35:12 -0500

    Update the user registration page with helpful text, UI

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -11,6 +11,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
@@ -93,84 +94,94 @@ class SignUpFormBase extends Component {
       username === '';
 
     return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Typography component="h1" variant="h5">
-          Sign Up
-        </Typography>
-        <form onSubmit={this.onSubmit}>
-          <TextField
-            onChange={this.onChange}
-            value={username}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            placeholder="Username"
-            autoComplete="username"
-            autoFocus
-          />
-          <TextField
-            onChange={this.onChange}
-            value={email}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            value={passwordOne}
-            onChange={this.onChange}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="passwordOne"
-            label="Password"
-            type="password"
-            id="passwordOne"
-            placeholder="Password"
-            autoComplete="current-password"
-          />
-          <TextField
-            value={passwordTwo}
-            onChange={this.onChange}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="passwordTwo"
-            label="Confirm Password"
-            type="password"
-            id="passwordOne"
-            placeholder="Confirm Password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox name="isAdmin" checked={isAdmin} 
-            onChange={this.onChangeCheckbox} value="is-admin" color="primary" />}
-            label="Is Admin"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            disabled={isInvalid}
-          >
+      <Container component="main" maxWidth="sm">
+        <Paper className="paper-sign-in">
+          
+          <CssBaseline />
+          <Typography component="h1" variant="h5">
             Sign Up
-          </Button>
-          {error && <p>{error.message}</p>}
+          </Typography>
+          <form onSubmit={this.onSubmit}>
+            <TextField
+              onChange={this.onChange}
+              value={username}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              placeholder="Username"
+              autoComplete="username"
+              helperText="Your username will be displayed publicly to identify your results."
+              autoFocus
+            />
+            
+            <TextField
+              onChange={this.onChange}
+              value={email}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              helperText="Your email address will be used for login and account management."
+            />
+            <TextField
+              value={passwordOne}
+              onChange={this.onChange}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="passwordOne"
+              label="Password"
+              type="password"
+              id="passwordOne"
+              placeholder="Password"
+              autoComplete="current-password"
+            />
+            <TextField
+              value={passwordTwo}
+              onChange={this.onChange}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="passwordTwo"
+              label="Confirm Password"
+              type="password"
+              id="passwordOne"
+              placeholder="Confirm Password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox name="isAdmin" checked={isAdmin} 
+              onChange={this.onChangeCheckbox} value="is-admin" color="primary" />}
+              label="Is Admin"
+            />
+            <p>
+              <small>Your data will not be used in any way other than for required functionality in this Pick'em league.
+                Your email address will not be visible to anybody.  Your username will be used to identify your league results.
+              </small>
+            </p>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              disabled={isInvalid}
+            >
+              Sign Up
+            </Button>
+            {error && <p>{error.message}</p>}
 
-        </form>
+          </form>
+        </Paper>
       </Container>
     );
   }
